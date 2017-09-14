67707571b5a8cbb4fb0aa94af43ccb48c1f608f3
Omar Ibrahim <ois2@ymail.com>
2017-09-14 21:35:58 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,1 +1,3 @@
-net use p: \\stor636404909669542276.file.core.windows.net\videos /u:AZURE\stor636404909669542276 5bmtpLDVKV98fqM7zM44LaaPeyWLQb/eY6wnTDAA8GQmShY/KXdxqkf/3gOzOwYESskDLSFJ5nPPaS6O0ysYsg==
+$azCopyPath =  "C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe"
+Set-Location $azCopyPath
+.\AzCopy.exe  /Source:https://stor636404909669542276.file.core.windows.net/videos /Dest:D:\videos\  /SourceKey:5bmtpLDVKV98fqM7zM44LaaPeyWLQb/eY6wnTDAA8GQmShY/KXdxqkf/3gOzOwYESskDLSFJ5nPPaS6O0ysYsg==  /Pattern:testfile.txt
