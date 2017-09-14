06762d6ea952cf8695f121911a0449477e8a8536
Omar Ibrahim <ois2@ymail.com>
2017-09-14 22:59:43 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,3 +1,3 @@
-$azCopyPath =  "C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe"
+$azCopyPath =  "C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy"
 Set-Location $azCopyPath
 .\AzCopy.exe  /Source:https://stor636404909669542276.file.core.windows.net/videos /Dest:D:\videos\  /SourceKey:5bmtpLDVKV98fqM7zM44LaaPeyWLQb/eY6wnTDAA8GQmShY/KXdxqkf/3gOzOwYESskDLSFJ5nPPaS6O0ysYsg==  /Pattern:testfile.txt
