c80571a7d671f45862ccf96a4206eebaa3f407be
Omar Ibrahim <ois2@ymail.com>
2017-09-16 22:38:25 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,11 +1,9 @@
 $azCopyPath =  "C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy"
 Set-Location $azCopyPath
+
 $source=$args[0]
 $dest=$args[1]
 $sourceKey=$args[2]
 $pattern=$args[3]
-$source
-$dest
-$sourceKey
-$pattern
+
 .\AzCopy.exe  /Source:$source /Dest:$dest  /SourceKey:$sourceKey  /Pattern:$pattern
