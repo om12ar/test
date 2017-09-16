ca2744b20696769ab376690ad8e7e600c221e4c1
Omar Ibrahim <ois2@ymail.com>
2017-09-16 20:00:02 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,5 +1,3 @@
-
-try{
 $azCopyPath =  "C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy"
 Set-Location $azCopyPath
 $source=$args[0]
@@ -8,8 +6,3 @@ $sourceKey=$args[2]
 $pattern=$args[3]
 
 .\AzCopy.exe  /Source:$source /Dest:$dest  /SourceKey:$sourceKey  /Pattern:$pattern
-}
-catch{
-    $exception = $_.Exception.Message
-    Out-File -FilePath 'c:\myscript.log' -Append -InputObject $exception
-}
