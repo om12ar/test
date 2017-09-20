94f48de71e82dddaeb15137bbc60c6ad6480c918
Omar Ibrahim <ois2@ymail.com>
2017-09-20 23:00:00 +0200

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,14 +1,1 @@
-$azCopyPath =  "C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy"
-Set-Location $azCopyPath
-
-$source=$args[0]
-$dest=$args[1]
-$sourceKey=$args[2]
-$pattern=$args[3]
-
-$source
-$dest
-$sourceKey
-$pattern
-
-.\AzCopy.exe  /Source:$source /Dest:$dest  /DestKey:$sourceKey  /Pattern:$pattern
+mkdir D:\working
